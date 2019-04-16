d8c117bb38ed891a57b3a604db9d1e33def19556
Samoila Alexandru <samoila.alexandru@gmail.com>
2019-04-16 10:50:02 +0300

    changed to bike

--- quotesbot/spiders/toscrape-xpath.py
+++ quotesbot/spiders/toscrape-xpath.py
@@ -9,14 +9,14 @@ class ToScrapeSpiderXPath(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for quote in response.xpath('//div[@class="quote"]'):
+        for quote in response.xpath('//div[@class="item_list"]'):
             yield {
-                'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
-                'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
-                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
+                'code': quote.xpath('./div[@class="ceny"]/text()').extract_first(),
+                'desc': quote.xpath('./div[@class="item_list_desc1"]/text()').extract_first(),
+                'stock': quote.xpath('.//div[@class="item_list_stav"]/img[@class="tool"]/src()').extract()
             }
 
-        next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
+        next_page_url = response.xpath('//li[@class="page"]/a/@href').extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
 
